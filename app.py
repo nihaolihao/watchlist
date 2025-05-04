b2d85eace74765a35cf80c2a0d4e810877f48d5a
lihaonihao <lihaonihao333@github.com>
2025-05-04 20:25:05 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask, url_for
+# -*- coding: utf-8 -*-
+
+from flask import Flask, url_for,render_template
 app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
 @app.route('/home')
-def hello():
-    return 'Welcome to My Watchlist!'
+def index():
+    # return 'Welcome to My Watchlist!'
+    return render_template('index.html',name=name,movies=movies)
 
 @app.route('/user/<name>')
 def user_page(name):
@@ -19,3 +22,23 @@ def test_url_for():
     print(url_for('test_url_for'))
     print(url_for('test_url_for', num=2))
     return 'Test page'
+
+# 定义虚拟数据
+name = 'lihaonihao'
+movies = [
+    {
+        'title': 'My Neighbor Totoro','year':'1988'
+    },
+    {
+        'title': 'Three Colours trilogy','year':'1993'
+    },
+    {
+        'title': 'Forrest Gump','year':'1994'
+    },
+    {
+        'title': 'Memento','year':'2000'
+    }
+]
+
+if __name__ == '__main__':
+    app.run(debug=True)
