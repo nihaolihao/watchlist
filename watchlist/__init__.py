88a52fff4e9dd370d0d3085cd36e41235bcb2c9f
lihaonihao <lihaonihao333@github.com>
2025-05-05 21:53:08 +0800

    Ready to deploy

--- watchlist/__init__.py
+++ watchlist/__init__.py
@@ -6,14 +6,14 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'dev'  # 设置密钥
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'dev')  # 设置密钥
 
 WIN = sys.platform.startswith('win')
 if WIN:
     prefix = 'sqlite:///'
 else:
     prefix ='sqlite:////'
-app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE','data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # 关闭对模型修改的监控
 
 db = SQLAlchemy(app) # 实例化数据库对象
