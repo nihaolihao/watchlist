99283862dfd42c09f104c5fbe3733d7fa61135ce
lihaonihao <lihaonihao333@github.com>
2025-05-31 18:38:18 +0800

    Add database support with Flask-SQLAlchemy

--- app.py
+++ app.py
@@ -1,23 +1,73 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+import sys
 
+import click
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import os
+import sys
+app = Flask(__name__)
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix ='sqlite:///'
+else:
+    prefix ='sqlite:////'
+app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+db = SQLAlchemy(app)
 @app.route('/')
 def index():
     # return 'Welcome to My Watchlist!'
 
-    return render_template('index.html',name=name,movies=movies)
-# 定义虚拟数据
+    user = User.query.first()
+    movies = Movie.query.all()
+    return render_template('index.html',user=user,movies=movies)
 
-name = 'Li Hao'
-movies = [
-    {'title': 'The Shawshank Redemption', 'year': 1994},
-    {'title': 'The Godfather', 'year': 1972},
-    {'title': 'The Dark Knight', 'year': 2008},
-    {'title': '12 Angry Men', 'year': 1957},
-    {'title': 'Schindler\'s List', 'year': 1993},
-    {'title': 'The Lord of the Rings: The Return of the King', 'year': 2003},
-    {'title': 'Pulp Fiction', 'year': 1994},
-    {'title': 'The Lord of the Rings: The Fellowship of the Ring', 'year': 2001},
-    {'title': 'The Good, the Bad and the Ugly', 'year': 1966},
-    {'title': 'Fight Club', 'year': 1999}
-]
+
+# 创建数据库模型
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(60))
+    year = db.Column(db.String(4))
+
+# 自定义命令initdb
+@app.cli.command()
+@click.option('--drop',is_flag=True,help='Create after drop.')
+def initdb(drop):
+    """Initialize the database."""
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+
+@app.cli.command()
+def forge():
+    """Generate fake data."""
+    db.create_all()
+
+    # 定义虚拟数据
+
+    name = 'Li Hao'
+    movies = [
+        {'title': 'The Shawshank Redemption', 'year': 1994},
+        {'title': 'The Godfather', 'year': 1972},
+        {'title': 'The Dark Knight', 'year': 2008},
+        {'title': '12 Angry Men', 'year': 1957},
+        {'title': 'Schindler\'s List', 'year': 1993},
+        {'title': 'The Lord of the Rings: The Return of the King', 'year': 2003},
+        {'title': 'Pulp Fiction', 'year': 1994},
+        {'title': 'The Lord of the Rings: The Fellowship of the Ring', 'year': 2001},
+        {'title': 'The Good, the Bad and the Ugly', 'year': 1966},
+        {'title': 'Fight Club', 'year': 1999}
+    ]
+
+    user = User(name=name)
+    db.session.add(user)
+    for m in movies:
+        movie = Movie(title=m['title'], year=m['year'])
+        db.session.add(movie)
+    db.session.commit()
+    click.echo('Done.')
